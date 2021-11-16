b0c7d9c003da9e252eaec653026fcbf4a4f830a1
smokost <smokost@gmail.com>
2021-11-16 16:40:47 +0700

    refactor message.py types

--- telegram/types/message.py
+++ telegram/types/message.py
@@ -1,189 +1,87 @@
 from dataclasses import dataclass
 from enum import Enum
 
+from telegram.types.message_content import build_message_content
 
-class TextParseModeTypes(Enum):
-    """Типы парсинга текстов через tdlib"""
-
-    HTML = {'@type': 'textParseModeHTML'}
-    MARKDOWN = {'@type': 'textParseModeMarkdown', 'version': 2}
+__all__ = ('MessageSenderType', 'MessageSender', 'MessageForwardInfo', 'Message')
 
 
-# class MessageContentType(Enum):
-#     """Типы контента сообщений"""
-#
-#     ANIMATION = 'messageAnimation'
-#     AUDIO = 'messageAudio'
-#     BASIC_GROUP_CHAT_CREATE = 'messageBasicGroupChatCreate'
-#     CALL = 'messageCall'
-#     CHAT_ADD_MEMBERS = 'messageChatAddMembers'
-#     CHAT_CHANGE_PHOTO = 'messageChatChangePhoto'
-#     CHAT_CHANGE_TITLE = 'messageChatChangeTitle'
-#     CHAT_DELETE_MEMBER = 'messageChatDeleteMember'
-#     CHAT_DELETE_PHOTO = 'messageChatDeletePhoto'
-#     CHAT_JOIN_BY_LINK = 'messageChatJoinByLink'
-#     CHAT_SET_TTL = 'messageChatSetTtl'
-#     CHAT_UPGRADE_FROM = 'messageChatUpgradeFrom'
-#     CHAT_UPGRADE_TO = 'messageChatUpgradeTo'
-#     CONTACT = 'messageContact'
-#     CONTACT_REGISTERED = 'messageContactRegistered'
-#     CUSTOM_SERVICE_ACTION = 'messageCustomServiceAction'
-#     DICE = 'messageDice'
-#     DOCUMENT = 'messageDocument'
-#     EXPIRED_PHOTO = 'messageExpiredPhoto'
-#     EXPIRED_VIDEO = 'messageExpiredVideo'
-#     GAME = 'messageGame'
-#     GAME_SCORE = 'messageGameScore'
-#     INVOICE = 'messageInvoice'
-#     LOCATION = 'messageLocation'
-#     PASSPORT_DATA_RECEIVED = 'messagePassportDataReceived'
-#     PASSPORT_DATA_SENT = 'messagePassportDataSent'
-#     PAYMENT_SUCCESSFUL = 'messagePaymentSuccessful'
-#     PAYMENT_SUCCESSFUL_BOT = 'messagePaymentSuccessfulBot'
-#     PHOTO = 'messagePhoto'
-#     PIN_MESSAGE = 'messagePinMessage'
-#     POLL = 'messagePoll'
-#     PROXIMITY_ALERT_TRIGGERED = 'messageProximityAlertTriggered'
-#     SCREENSHOT_TAKEN = 'messageScreenshotTaken'
-#     STICKER = 'messageSticker'
-#     SUPERGROUP_CHAT_CREATE = 'messageSupergroupChatCreate'
-#     TEXT = 'messageText'
-#     UNSUPPORTED = 'messageUnsupported'
-#     VENUE = 'messageVenue'
-#     VIDEO = 'messageVideo'
-#     VIDEO_NOTE = 'messageVideoNote'
-#     VOICE_NOTE = 'messageVoiceNote'
-#     WEBSITE_CONNECTED = 'messageWebsiteConnected'
-#
-#     IGNORED = (
-#         BASIC_GROUP_CHAT_CREATE,
-#         CALL,
-#         CHAT_CHANGE_PHOTO,
-#         CHAT_CHANGE_TITLE,
-#         CHAT_DELETE_PHOTO,
-#         CHAT_SET_TTL,
-#         CHAT_UPGRADE_FROM,
-#         CHAT_UPGRADE_TO,
-#         CONTACT,
-#         CONTACT_REGISTERED,
-#         CUSTOM_SERVICE_ACTION,
-#         DICE,
-#         EXPIRED_PHOTO,
-#         EXPIRED_VIDEO,
-#         GAME,
-#         GAME_SCORE,
-#         PASSPORT_DATA_RECEIVED,
-#         PASSPORT_DATA_SENT,
-#         PAYMENT_SUCCESSFUL,
-#         PAYMENT_SUCCESSFUL_BOT,
-#         PIN_MESSAGE,
-#         PROXIMITY_ALERT_TRIGGERED,
-#         SCREENSHOT_TAKEN,
-#         SUPERGROUP_CHAT_CREATE,
-#     )
-#
-#
-# class MessageSenderType(Enum):
-#     """Типы отправителей сообщения"""
-#
-#     CHAT = 0
-#     USER = 1
-#
-#
-# @dataclass
-# class MessageSender:
-#     id: int
-#     type: MessageSenderType
-#
-#
-# @dataclass
-# class ForwardInfo:
-#     """Информация об внутреннем отправителе сообщения"""
-#
-#     date: int
-#     from_chat_id: int
-#     from_message_id: int
-#
-#
-# class TextEntityType(Enum):
-#     """Типы элементов форматирования текста"""
-#
-#     BANK_CARD_NUMBER = 'textEntityTypeBankCardNumber'
-#     BOLD = 'textEntityTypeBold'
-#     BOT_COMMAND = 'textEntityTypeBotCommand'
-#     CASHTAG = 'textEntityTypeCashtag'
-#     CODE = 'textEntityTypeCode'
-#     EMAIL_ADDRESS = 'textEntityTypeEmailAddress'
-#     HASHTAG = 'textEntityTypeHashtag'
-#     ITALIC = 'textEntityTypeItalic'
-#     MENTION = 'textEntityTypeMention'
-#     MENTION_NAME = 'textEntityTypeMentionName'
-#     PHONE_NUMBER = 'textEntityTypePhoneNumber'
-#     PRE = 'textEntityTypePre'
-#     PRE_CODE = 'textEntityTypePreCode'
-#     STRIKETHROUGH = 'textEntityTypeStrikethrough'
-#     TEXT_URL = 'textEntityTypeTextUrl'
-#     UNDERLINE = 'textEntityTypeUnderline'
-#     URL = 'textEntityTypeUrl'
-#
-#
-# @dataclass
-# class TextEntity:
-#     """Элемент форматирования текста"""
-#
-#     offset: int
-#     length: int
-#     type: TextEntityType
-#     data: str
-#
-#
-# @dataclass
-# class FormattedText:
-#     """Форматированный текст"""
-#
-#     text: str
-#     entities: [TextEntity]
+class MessageSenderType(Enum):
+    """Тип отправителя"""
+
+    CHAT = 'messageSenderChat'
+    USER = 'messageSenderUser'
 
 
-@dataclass(frozen=True)
+@dataclass
+class MessageSender:
+    """Отправитель сообщения"""
+
+    raw: dict
+    type: MessageSenderType = None
+    id: int = None
+
+    def __post_init__(self):
+        self.type = MessageSenderType(self.raw.pop('@type'))
+        if self.type == MessageSenderType.USER:
+            self.id = self.raw.pop('user_id')
+        else:
+            self.id = self.raw.pop('chat_id')
+
+
+@dataclass
+class MessageForwardInfo:
+    """Информация об внутреннем отправителе сообщения"""
+
+    raw: dict
+    date: int = None
+    from_chat_id: int = None
+    from_message_id: int = None
+
+    def __post_init__(self):
+        self.date = self.raw.pop('date')
+        self.from_chat_id = self.raw.pop('from_chat_id')
+        self.from_message_id = self.raw.pop('from_message_id')
+
+
+@dataclass
 class Message:
     """Сообщение из обновления телеграм"""
 
-    # id: int
-    # chat_id: int
-    # type: MessageContentType
-    #
-    # message_sender: MessageSender
-    # date: int
-    #
-    # is_outgoing: bool
-    # is_pinned: bool
-    # can_be_edited: bool
-    # can_be_forwarded: bool
-    # can_be_deleted_only_for_self: bool
-    # can_be_deleted_for_all_users: bool
-    # can_get_statistics: bool
-    # is_channel_post: bool
-    # contains_unread_mention: bool
-    # edit_date: bool
-    #
-    # forward_info: ForwardInfo
-    #
-    # caption: FormattedText = None
-    # text: FormattedText = None
-    # new_member_ids: list = None
-    # delete_member_id: int = None
-    #
-    # # animation: Animation = None
-    # # audio: Audio = None
-    # # contact: Contact = None
-    # TODO: Fill the content types fields
+    raw: dict
 
-    @classmethod
-    def build(cls, message: dict) -> 'Message':
-        pass
-        # _type = message.pop('@type')
-        # return cls(type=_type, **message)
+    id: int = None
+    chat_id: int = None
+    date: int = None
+    edit_date: int = None
+    reply_in_chat_id: int = None
+    reply_to_message_id: int = None
 
+    is_outgoing: bool = None
+    is_pinned: bool = None
+    can_be_edited: bool = None
+    can_be_forwarded: bool = None
+    can_be_deleted_only_for_self: bool = None
+    can_be_deleted_for_all_users: bool = None
+    can_get_statistics: bool = None
+    is_channel_post: bool = None
+    contains_unread_mention: bool = None
 
-build_message = Message.build
+    sender: MessageSender = None
+    forward_info: MessageForwardInfo = None
+    content: object = None
+    reply_markup: dict = None
+
+    def __post_init__(self):
+        self.sender = MessageSender(self.raw.pop('sender'))
+        forward_info = self.raw.pop('forward_info', None)
+        if forward_info:
+            self.forward_info = MessageForwardInfo(forward_info)
+        self.content = build_message_content(self.raw.pop('content'))
+
+        keys = self.raw.keys()
+        keys = list(keys)
+        for key in keys:
+            if hasattr(self, key):
+                value = self.raw.pop(key)
+                setattr(self, key, value)
